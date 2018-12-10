8869ab54901c0d8dddafda75d87c2c23348a6409
Brandon Patterson <brandnpatterson@gmail.com>
2018-12-10 17:02:34 -0600

    test herokup

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 require 'vendor/autoload.php';
 require 'conf/conf.php';
 require 'app/classes/Mailer.php';
-require 'templates/index.php';
+header('Access-Control-Allow-Origin: *');
+
+// require 'templates/index.php';

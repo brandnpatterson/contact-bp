6d853a1c75b70849f50fcc5d3614b810d8f55a8b
Brandon Patterson <brandnpatterson@gmail.com>
2018-12-18 13:43:44 -0600

    use public folder

--- public/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <a href="https://github.com/brandnpatterson" target="_blank">&copy; <?=date('Y')?> Brandon Patterson</a>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
-    <script src="https://unpkg.com/coriander@1.3.8/dist/coriander.min.js"></script>
-    <script src="public/js/index.js"></script>
-</footer>
--- public/js/index.js
+++ public/js/index.js
@@ -2,21 +2,35 @@ var form = document.querySelector('.form');
 var mailerAlert = document.querySelector('.mailer-alert');
 var mailerCheck = document.querySelector('.mailer-check');
 
+function showMessage(message) {
+  mailerAlert.querySelector('p').textContent = message;
+}
+
 coriander(form, {
   onChange: true,
   onSubmit: function(data) {
     var formData = new FormData(data.form);
+
     axios
       .post(data.form.action, formData)
       .then(function(res) {
         mailerAlert.classList.add('alert-success');
-        mailerAlert.querySelector('p').textContent = res.data.message;
         mailerCheck.classList.remove('hidden');
+
+        showMessage(res.data.message);
       })
       .catch(function(err) {
         mailerAlert.classList.add('alert-success');
-        mailerAlert.querySelector('p').textContent = err.data.message;
         mailerCheck.classList.remove('hidden');
+
+        if (err.data.message) {
+          showMessage(err.data.message);
+        } else {
+          showMessage('Your email was not sent');
+        }
+      })
+      .then(function() {
+        window.scrollTo(0, 0);
       });
   }
 });
